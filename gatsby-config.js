4321a8249c331d4910b178e70565f55bab636df3
ALEX <zhangxiangjie@zhangxiangjies-MacBook.local>
2019-09-12 00:11:55 +0800

    docs: update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Uke blog`,
+    author: `Alex Zhang`,
+    description: `A blog of alex zhang.`,
+    siteUrl: `https://ukelli.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `sanget`,
     },
   },
   plugins: [
