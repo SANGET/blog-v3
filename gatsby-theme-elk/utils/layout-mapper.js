d6c1729b126e4e002b3aeb788067bb1dadf4681b
sanget <sanget@sangetdeMacBook-Pro.local>
2019-09-20 23:31:35 +0800

    fix

--- gatsby-theme-elk/utils/layout-mapper.js
+++ gatsby-theme-elk/utils/layout-mapper.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const layoutMapper = {
   'post': path.resolve(__dirname, `../src/templates/blog-post.js`),
-  'tags': path.resolve(__dirname, './src/templates/tags.js'),
-  'blogList': path.resolve(__dirname, './src/templates/blog-list.js'),
+  'tags': path.resolve(__dirname, '../src/templates/tags.js'),
+  'blogList': path.resolve(__dirname, '../src/templates/blog-list.js'),
 };
 
 module.exports = layoutMapper;
