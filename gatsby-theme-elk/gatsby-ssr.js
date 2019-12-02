5c62645bdea3fb2d9e75f004485c05265f8f0efc
sanget <zh.sanget@gmail.com>
2019-12-02 23:59:14 +0800

    v1.0.1-rc1.4

--- gatsby-theme-elk/gatsby-ssr.js
+++ gatsby-theme-elk/gatsby-ssr.js
@@ -7,20 +7,18 @@ export const onRenderBody = ({
   setPostBodyComponents, setPreBodyComponents, setHeadComponents,
 }) => {
   setPreBodyComponents([
-    <LoaderDOM />,
+    <LoaderDOM key="1" />,
   ]);
   setPostBodyComponents([
-    <>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.3.1/css/all.min.css" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css"></link>
+    <link key="1" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.3.1/css/all.min.css" />,
+    <link key="2" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@3.0.1/github-markdown.min.css"></link>,
 
-      <script src="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/prism.min.js" defer></script>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/themes/prism.css"></link>
+    <script key="3" src="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/prism.min.js" defer></script>,
+    <link key="4" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/themes/prism.css"></link>,
 
-      <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js" defer></script>
+    <script key="5" src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js" defer></script>,
 
-      <script src="https://cdn.jsdelivr.net/gh/stylehatch/photoset-grid@master/jquery.photoset-grid.min.js" defer></script>
-    </>,
+    <script key="6" src="https://cdn.jsdelivr.net/gh/stylehatch/photoset-grid@master/jquery.photoset-grid.min.js" defer></script>,
   ]);
 };
 
