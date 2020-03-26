4618fc25a0e6b161b1939dec78425b836b4c0eee
sanget <zh.sanget@gmail.com>
2020-03-26 23:16:40 +0800

    Organized files

--- gatsby-theme-elk/src/components/layout-wrapper.tsx
+++ gatsby-theme-elk/src/components/layout-wrapper.tsx
@@ -84,7 +84,7 @@ const Wrapper = ({ children, props }) => {
   React.useEffect(() => {
     const loadingDOM = document.querySelector('#loadingBg');
     if (loadingDOM) document.body.removeChild(loadingDOM);
-    Sentry && Sentry.init({ dsn: 'https://82e677b839d04307ac5ce8099381c1a9@sentry.io/5170362' });
+    window.Sentry?.init({ dsn: 'https://82e677b839d04307ac5ce8099381c1a9@sentry.io/5170362' });
     // myUndefinedFunction();
   }, []);
 
